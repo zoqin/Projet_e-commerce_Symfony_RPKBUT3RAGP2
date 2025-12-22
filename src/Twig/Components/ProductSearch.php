f5d544a215b237088251c97957b1f491772d8b2b
zoqin <zoqinrobinpk@hotmail.fr>
2025-12-22 14:23:35 +0100

    Modification ProductSearch.php

--- src/Twig/Components/ProductSearch.php
+++ src/Twig/Components/ProductSearch.php
@@ -2,7 +2,10 @@
 
 namespace App\Twig\Components;
 
+use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\UX\LiveComponent\Attribute\AsLiveComponent;
 use Symfony\UX\LiveComponent\Attribute\LiveProp;
 use Symfony\UX\LiveComponent\DefaultActionTrait;
@@ -15,7 +18,18 @@ final class ProductSearch
     #[LiveProp(writable:true, url: true)]
     public string $query = '';
 
-    public function __construct(private ProductRepository $productRepository) {}
+    #[LiveProp(writable:true, url: true)]
+    public array $selectedCategories = [];
+
+    public function __construct(
+        private ProductRepository $productRepository,
+        private CategoryRepository $categoryRepository,
+    ) {}
+
+    public function getCategories(): array
+    {
+        return $this->categoryRepository->findAll();
+    }
 
     public function getProducts(): array
     {
@@ -23,10 +37,21 @@ final class ProductSearch
             return $this->productRepository->findBy([], ['id' => 'DESC'], 10);
         }
 
-        return $this->productRepository
-            ->createQueryBuilder('p')
-            ->where('p.name LIKE :query')
-            ->setParameter('query', '%' . $this->query . '%')
+        $qb = $this->productRepository->createQueryBuilder('p');
+
+        if(!empty($this->query)) {
+            $qb->andWhere('p.name LIKE :query')
+                ->setParameter('query', '%'.$this->query.'%');
+        }
+
+        if(!empty($this->selectedCategories)) {
+            $qb->join('p.category', 'c')
+                ->andWhere('c.id IN (:categories)')
+                ->setParameter('categories', $this->selectedCategories);
+        }
+
+        return $qb->orderBy('p.id', 'DESC')
+            ->setMaxResults(9)
             ->getQuery()
             ->getResult();
     }
